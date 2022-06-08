7e8f105874b474b84ca11de69fbef4315aba1926
Martin Rauscher <hades32@gmail.com>
2022-06-08 19:39:26 +0200

    initial

---
+++ Assets/Scripts/ExternalScriptBehavior.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+namespace Assets.Scripts.Core
+{
+    public class ExternalScriptBehavior
+    {
+#if UNITY_WEBGL
+        [DllImport("__Internal")]
+        public static extern void Log(string str);
+
+        [DllImport("__Internal")]
+        public static extern bool IsSecure();
+
+        [DllImport("__Internal")]
+        public static extern string Hostname();
+
+        [DllImport("__Internal")]
+        public static extern int Port();
+
+#else
+        public static void Log(string str)
+        {
+            Debug.Log(str);
+        }
+        public static bool IsSecure()
+        {
+            return false;
+        }
+        public static string Hostname()
+        {
+            return "localhost";
+        }
+        public static int Port()
+        {
+            return 7778;
+        }
+#endif
+
+    }
+}
